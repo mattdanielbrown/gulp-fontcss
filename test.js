415f9ecfbdb2231ef7a3898d07522776e4dcc274
janhuenermann <j.huenermann@icloud.com>
2016-12-30 00:29:52 +0100

    Converted indentation

--- test.js
+++ test.js
@@ -2,16 +2,16 @@ var gulp = require('gulp')
 var fontcss = require('.')
 
 gulp.src('test-src/*/*')
-	.pipe(fontcss({
+    .pipe(fontcss({
 
-		modifyFontSheet: function (sheet, file) {
-			switch(sheet.family.toLowerCase()) {
-				case 'crimsontext': sheet.family = 'Crimson Text'; break;
-				default: break;
-			}
+        modifyFontSheet: function (sheet, file) {
+            switch(sheet.family.toLowerCase()) {
+                case 'crimsontext': sheet.family = 'Crimson Text'; break;
+                default: break;
+            }
 
-			return sheet;
-		}
+            return sheet;
+        }
 
-	}))
-	.pipe(gulp.dest('test-dest'));
+    }))
+    .pipe(gulp.dest('test-dest'));
